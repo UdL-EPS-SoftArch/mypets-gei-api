ee9a28226d69b01960a36fb067b56d42540548d5
Roberto García <roberto@rhizomik.net>
2024-02-13 13:25:19 +0100

    Initial commit

---
+++ src/test/java/cat/udl/eps/softarch/demo/MyPetsTests.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.softarch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetsTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
