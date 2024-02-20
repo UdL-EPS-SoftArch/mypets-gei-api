3787469b0a79253e581f6c44efc6e06a72524da3
globox97 <O46001232@studium.unict.it>
2024-02-20 16:18:19 +0100

    Basic creation for the classes I've been assigned

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Location.java
@@ -0,0 +1,11 @@
+package cat.udl.eps.softarch.demo.domain;
+
+public class Location {
+    private int id;
+    private String address;
+    private float latitude;
+    private float longitude;
+    private String province;
+    private String municipality;
+    private String postalCode;
+}
