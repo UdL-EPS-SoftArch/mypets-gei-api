b9b822df2c7623ab15b2be9c207569fd97e547c3
Pol Escolà Curcó <polescola3000@gmail.com>
2024-02-20 16:47:54 +0100

    UriEntity extended

--- src/main/java/cat/udl/eps/softarch/demo/domain/SocialNetworks.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/SocialNetworks.java
@@ -1,10 +1,13 @@
 package cat.udl.eps.softarch.demo.domain;
 
-public class SocialNetworks {
-    int id;
+public class SocialNetworks extends UriEntity<Long>{
+    Long id;
     String instagram;
     String twitter;
 
 
-
+    @Override
+    public Long getId() {
+        return id;
+    }
 }
