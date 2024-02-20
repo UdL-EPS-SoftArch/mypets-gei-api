1506bf81f3559a9fd2022ac8ab7c228c8a019ca5
Antt02 <antoniolopez18022002@gmail.com>
2024-02-20 15:57:52 +0100

    Created Pet class

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Pet.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.softarch.demo.domain;
+
+public class Pet {
+    int id;
+    String name;
+    boolean isAdopted;
+    String color;
+    String size;
+    Double weight;
+    String age;
+    String description;
+    String breed;
+}
