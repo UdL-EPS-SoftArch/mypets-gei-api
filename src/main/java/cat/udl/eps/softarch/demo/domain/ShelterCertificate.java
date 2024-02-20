cbd9dd37aeb04a216497a9f389d35df71700890e
Pere Muñoz <73690199+peremunoz@users.noreply.github.com>
2024-02-20 17:14:01 +0100

    Create ShelterCertificate.java

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ShelterCertificate.java
@@ -0,0 +1,10 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import java.util.Date;
+
+public class ShelterCertificate extends UriEntity<Long> {
+    
+    private Long id;
+    private Date expirationDate;
+    
+}
