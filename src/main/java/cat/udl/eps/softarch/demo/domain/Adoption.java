2c4cb969be0d951f026b71c71e04d7c2b09c5645
Codinab <alex.codinab@gmail.com>
2024-02-20 16:00:07 +0100

    Adoption.java
    -Mario Fernandez
    -Àlex Codina

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Adoption.java
@@ -0,0 +1,62 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import java.util.Date;
+
+public class Adoption {
+    private Integer id;
+    private String type;
+    private Boolean confirmed;
+    private Date startDate;
+    private Date endDate;
+
+    // Constructor
+    public Adoption(Integer id, String type, Boolean confirmed, Date startDate, Date endDate) {
+        this.id = id;
+        this.type = type;
+        this.confirmed = confirmed;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // Getters and Setters
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(Boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
