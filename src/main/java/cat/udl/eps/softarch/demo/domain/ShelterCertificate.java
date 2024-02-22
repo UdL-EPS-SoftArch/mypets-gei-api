a9654ac1f4af655ff4b596e97d2d6d1c76834d3d
Faysal Badaoui <faysalbadaouimahdad@gmail.com>
2024-02-22 15:52:43 +0100

    labels to class

--- src/main/java/cat/udl/eps/softarch/demo/domain/ShelterCertificate.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ShelterCertificate.java
@@ -1,11 +1,24 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import java.time.ZonedDateTime;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class ShelterCertificate extends UriEntity<Long> {
-    
+
+    @Id
+    @GeneratedValue()
     private Long id;
-    private Date expirationDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @NotNull
+    private ZonedDateTime expirationDate;
 
     @Override
     public Long getId() {
