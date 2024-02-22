6203dcad6a9a597ce9d86bfe4c74e3ed04c26ae4
Faysal Badaoui <faysalbadaouimahdad@gmail.com>
2024-02-22 16:06:13 +0100

    added relationship to shelter

--- src/main/java/cat/udl/eps/softarch/demo/domain/ShelterVolunteer.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ShelterVolunteer.java
@@ -1,8 +1,12 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class ShelterVolunteer extends User {
-
+    @JsonIdentityReference(alwaysAsId = true)
+    @ManyToOne
+    private Shelter userShelter;
 }
