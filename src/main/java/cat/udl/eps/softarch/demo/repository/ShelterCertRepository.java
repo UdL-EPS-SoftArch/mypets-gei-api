c4d0595c0f7137ab26bac6ca7518826a3ef7e975
Faysal Badaoui <faysalbadaouimahdad@gmail.com>
2024-02-22 16:12:08 +0100

    created repository

---
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ShelterCertRepository.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.softarch.demo.repository;
+
+import cat.udl.eps.softarch.demo.domain.Shelter;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ShelterCertRepository extends CrudRepository<Shelter, Long>, PagingAndSortingRepository<Shelter, Long> {
+
+}
