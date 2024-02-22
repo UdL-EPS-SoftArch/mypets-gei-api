4658894c84788faa834d496c03ea217ca4bacd2b
Faysal Badaoui <faysalbadaouimahdad@gmail.com>
2024-02-22 16:45:42 +0100

    added data and id

--- src/main/java/cat/udl/eps/softarch/demo/domain/ShelterVolunteer.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ShelterVolunteer.java
@@ -2,10 +2,17 @@ package cat.udl.eps.softarch.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 @Entity
 public class ShelterVolunteer extends User {
+    @Id
+    private long Id;
     @JsonIdentityReference(alwaysAsId = true)
     @ManyToOne
     private Shelter userShelter;
