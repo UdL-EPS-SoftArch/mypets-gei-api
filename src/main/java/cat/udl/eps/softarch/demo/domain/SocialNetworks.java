82a7f9d7486fd26efc87fb0665f38f98587faf79
Roberto García <roberto@rhizomik.net>
2024-02-22 09:54:19 +0100

    Annotate entity to make it persistent

--- src/main/java/cat/udl/eps/softarch/demo/domain/SocialNetworks.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/SocialNetworks.java
@@ -1,13 +1,19 @@
 package cat.udl.eps.softarch.demo.domain;
 
-public class SocialNetworks extends UriEntity<Long>{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class SocialNetworks extends UriEntity<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
     String instagram;
     String twitter;
-
-
-    @Override
-    public Long getId() {
-        return id;
-    }
 }
