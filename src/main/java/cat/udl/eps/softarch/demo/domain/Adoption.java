b7c40fa630fb954dd7f5a119b6f776ae81536bd2
Codinab <alex.codinab@gmail.com>
2024-02-22 15:35:26 +0100

    Added more annotations
    -Mario Fernandez
    -Àlex Codina

--- src/main/java/cat/udl/eps/softarch/demo/domain/Adoption.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Adoption.java
@@ -1,5 +1,8 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,9 +13,11 @@ import java.time.ZonedDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@Entity
 public class Adoption extends UriEntity<Long> {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @NotBlank
