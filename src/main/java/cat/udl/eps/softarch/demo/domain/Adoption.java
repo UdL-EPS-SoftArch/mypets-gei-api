c1ebda50549bd55bb3ead667348ece8f75bff157
Codinab <alex.codinab@gmail.com>
2024-02-22 15:24:49 +0100

    Added annotations
    -Mario Fernandez
    -Àlex Codina

--- src/main/java/cat/udl/eps/softarch/demo/domain/Adoption.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Adoption.java
@@ -1,58 +1,28 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.util.Date;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class Adoption extends UriEntity<Long> {
-    private final Long id;
+    
+    @Id
+    private Long id;
+    
+    @NotBlank
     private String type;
+    
+    @NotBlank
     private Boolean confirmed;
+    
+    @NotBlank
     private Date startDate;
+    
+    
     private Date endDate;
-
-    // Constructor
-    public Adoption(Long id, String type, Boolean confirmed, Date startDate, Date endDate) {
-        this.id = id;
-        this.type = type;
-        this.confirmed = confirmed;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
-
-    // Getters and Setters
-    
-    public Long getId() {
-        return id;
-    }
-    
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Boolean getConfirmed() {
-        return confirmed;
-    }
-
-    public void setConfirmed(Boolean confirmed) {
-        this.confirmed = confirmed;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
 }
