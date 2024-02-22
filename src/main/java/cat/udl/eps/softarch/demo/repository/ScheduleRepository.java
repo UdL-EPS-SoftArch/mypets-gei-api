8c9314f1372b8359a8c088a6fdde4d96c14167ed
Sebastian Jitaru <sjitaru29@gmail.com>
2024-02-22 16:44:36 +0100

    Updated repo

--- src/main/java/cat/udl/eps/softarch/demo/repository/ScheduleRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ScheduleRepository.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Schedule;
+import cat.udl.eps.softarch.demo.domain.Shelter;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,6 +11,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface ScheduleRepository extends CrudRepository<Schedule, Long>, PagingAndSortingRepository<Schedule, Long>{
-    List<Schedule> findByIdContaining(@Param("text") String text);
+
+    //Get list of scheduled of a specific shelter
+    List<Schedule> findByAvailable(@Param("shelter") Shelter shelter);
 
 }
