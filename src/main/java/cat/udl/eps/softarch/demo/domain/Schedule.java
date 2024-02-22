8154927c6bb92aa0da4525cde0e541756b10c1b4
Roberto García <roberto@rhizomik.net>
2024-02-22 14:52:07 +0100

    Fix annotations and remove unnecessary getter

--- src/main/java/cat/udl/eps/softarch/demo/domain/Schedule.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Schedule.java
@@ -2,33 +2,23 @@ package cat.udl.eps.softarch.demo.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.util.Date;
 
-
 @Entity
-@Table(name = "Schedule") //Avoid collision with system table User
 @Data
 @EqualsAndHashCode(callSuper = true)
-
 public class Schedule extends UriEntity<Long> {
 
     @Id
-    private long id;
+    private Long id;
 
     @NotNull
     private Date start;
 
     @NotNull
     private Date end;
-
-
-    @Override
-    public Long getId() {
-        return id;
-    }
 }
