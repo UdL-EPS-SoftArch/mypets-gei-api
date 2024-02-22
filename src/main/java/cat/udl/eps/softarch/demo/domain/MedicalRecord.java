854ff55257f508d884c004f02389a11f27ad12de
Codinab <alex.codinab@gmail.com>
2024-02-22 15:43:09 +0100

    Added annotations
    -Mario Fernandez
    -Àlex Codina

--- src/main/java/cat/udl/eps/softarch/demo/domain/MedicalRecord.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/MedicalRecord.java
@@ -1,49 +1,31 @@
 package cat.udl.eps.softarch.demo.domain;
 
-import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import java.time.ZonedDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
 public class MedicalRecord extends UriEntity<Long> {
-    private final Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
     private String issue;
+
+    @NotBlank
     private String description;
-    private Date date;
-    
-    
 
-    // Constructor
-    public MedicalRecord(Long id, String issue, String description, Date date) {
-        this.id = id;
-        this.issue = issue;
-        this.description = description;
-        this.date = date;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public String getIssue() {
-        return issue;
-    }
-
-    public void setIssue(String issue) {
-        this.issue = issue;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
+    @NotNull
+    private ZonedDateTime date;
 }
