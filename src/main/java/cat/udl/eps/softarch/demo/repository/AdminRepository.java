3d186c5c612a0b6f5b808dccdb18fda76ae74162
Ant <antoniolopez18022002@gmail.com>
2024-02-22 16:04:59 +0100

    admin repository

--- src/main/java/cat/udl/eps/softarch/demo/repository/AdminRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/AdminRepository.java
@@ -10,16 +10,5 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AdminRepository extends CrudRepository<Admin, Long>, PagingAndSortingRepository<Admin, Long> {
-
-    /* Interface provides automatically, as defined in
-     * https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
-     * and
-     * https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html
-     * the methods: count, delete, deleteAll, deleteById, existsById, findAll, findAllById, findById, save, saveAll,...
-     *
-     * Additional methods like findByUsernameContaining can be defined following:
-     * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
-     */
-
     List<User> findByIdContaining(@Param("text") String text);
 }
