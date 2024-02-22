01053528e56e135bc21ec33d395e2cdbe96236e1
globox97 <O46001232@studium.unict.it>
2024-02-22 16:18:31 +0100

    crud-Client

--- src/main/java/cat/udl/eps/softarch/demo/domain/Client.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Client.java
@@ -1,15 +1,21 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 
+import java.util.Collection;
+
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class Client extends User {
-
+    @Override
+    @ElementCollection
+    public Collection<GrantedAuthority> getAuthorities(){
+        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_CLIENT");
+    }
 }
