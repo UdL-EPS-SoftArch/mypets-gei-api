b773e2b27ba60246d1d5ed70ee3437e024568d66
globox97 <O46001232@studium.unict.it>
2024-02-29 15:38:05 +0100

    crud-Location

--- src/main/java/cat/udl/eps/softarch/demo/repository/LocationRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/LocationRepository.java
@@ -1,15 +1,13 @@
 package cat.udl.eps.softarch.demo.repository;
 
-import cat.udl.eps.softarch.demo.domain.*;
+import cat.udl.eps.softarch.demo.domain.Location;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
 @RepositoryRestResource
 public interface LocationRepository extends CrudRepository<Location, String>, PagingAndSortingRepository<Location, String> {
 
-  Location findByIdContaining(@Param("text") String text);
+  /*Location findByIdContaining(@Param("text") String text);*/
 }
