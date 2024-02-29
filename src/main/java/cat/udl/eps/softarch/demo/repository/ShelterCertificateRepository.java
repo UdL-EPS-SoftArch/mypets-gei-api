532d52b40cbe1b5c21fa74344b8b4693c14e5a58
faysalbadaoui <faysal@faysalbadaoui.com>
2024-02-29 14:29:27 +0100

    rename to complete name

--- src/main/java/cat/udl/eps/softarch/demo/repository/ShelterCertificateRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ShelterCertificateRepository.java
@@ -4,6 +4,6 @@ import cat.udl.eps.softarch.demo.domain.Shelter;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ShelterCertRepository extends CrudRepository<Shelter, Long>, PagingAndSortingRepository<Shelter, Long> {
+public interface ShelterCertificateRepository extends CrudRepository<Shelter, Long>, PagingAndSortingRepository<Shelter, Long> {
 
 }
