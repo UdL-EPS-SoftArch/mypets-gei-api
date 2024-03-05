ebd512e503e48615b2fe26ae7f2609ede23d390c
Sebastian Jitaru <sjitaru29@gmail.com>
2024-03-05 16:03:26 +0100

    First scenario implemented

--- src/test/java/cat/udl/eps/softarch/demo/steps/CreateShelterStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/CreateShelterStepDefs.java
@@ -1,4 +1,40 @@
 package cat.udl.eps.softarch.demo.steps;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.repository.UserRepository;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 public class CreateShelterStepDefs {
+
+    @Given("^There is no shelter registered with the name \"([^\"]*)\" and location \"([^\"]*)\"$")
+    public void thereIsNoShelterRegisteredWithTheNameAndLocation(String name, String location) {
+        Assert.assertFalse("Shelter with name \""
+                + name + "\" and location \"" + location + "\" shouldn't exist",
+                shelterRepository.existsByNameAndLocation(name, location));
+    }
+
+    @And("I am a shelter manager")
+    public void iAmAShelterManager() {
+        
+    }
+
+    @When("I attempt to create a new shelter with name {string} and location {string}")
+    public void iAttemptToCreateANewShelterWithNameAndLocation(String arg0, String arg1) {
+        
+    }
+
+    @And("The response contains a success message confirming the shelter was created")
+    public void theResponseContainsASuccessMessageConfirmingTheShelterWasCreated() {
+    }
 }
--- src/test/resources/features/CreateShelter.feature
+++ src/test/resources/features/CreateShelter.feature
@@ -1,4 +1,11 @@
-Feature: Join Shelter
-  In order to use the app
-  As a user
-  I want to register myself and get an account
+Feature: Create Shelter
+  In order to provide temporary housing for animals
+  As a shelter manager
+  I want to create shelters and manage their information
+
+  Scenario: Create a new shelter
+    Given There is no shelter registered with the name "shelter" and location "city"
+    And I am a shelter manager
+    When I attempt to create a new shelter with name "shelter" and location "city"
+    Then The response code is 201
+    And The response contains a success message confirming the shelter was created
