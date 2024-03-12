8f85e7b8511fd715b0eefed2a1ea8dcbcfc26c9e
marioferro <marioferro2002@gmail.com>
2024-03-12 15:06:30 +0100

    Creation of features file and steps file

---
+++ src/test/java/cat/udl/eps/softarch/demo/steps/ValidateAdoptionStepDefs.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.softarch.demo.steps;
+
+import io.cucumber.java.en.Given;
+
+public class ValidateAdoptionStepDefs {
+    @Given("There is a pending adoption request for pet with ID {string} by user {string}")
+    public void thereIsAPendingAdoptionRequestForPetWithIDByUser(String arg0, String arg1) {
+    }
+}
