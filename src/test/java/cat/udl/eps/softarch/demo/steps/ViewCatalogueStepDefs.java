3cebf6cf7c1812babf826185272d3fa8ef57bec7
globox97 <O46001232@studium.unict.it>
2024-03-13 18:32:21 +0100

    Feature View Pets Catalogue - implemented two scenarios

--- src/test/java/cat/udl/eps/softarch/demo/steps/ViewCatalogueStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/ViewCatalogueStepDefs.java
@@ -1,18 +1,32 @@
 package cat.udl.eps.softarch.demo.steps;
 
-import cat.udl.eps.softarch.demo.repository.ShelterRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import cat.udl.eps.softarch.demo.repository.PetRepository;
 import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 public class ViewCatalogueStepDefs {
+
     @Autowired
     private StepDefs stepDefs;
 
     @Autowired
-    private ShelterRepository shelterRepository;
+    private PetRepository petRepository;
 
     @When("I request the Catalogue")
     public void viewCatalogue() throws Exception {
+        stepDefs.result = stepDefs.mockMvc.perform(get("/pets/search?isAdopted=false")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
 
+    @When("I request the Catalogue with breed {string}")
+    public void viewCataloguewithBreed(String breed) throws Exception{
+        stepDefs.result = stepDefs.mockMvc.perform(get("/pets/search?breed={breed}", breed)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
     }
 }
+
--- src/test/resources/features/ViewCatalogue.feature
+++ src/test/resources/features/ViewCatalogue.feature
@@ -7,16 +7,22 @@ Feature: View Pets Catalogue
   Background:
     Given there is a registered client with username "client" and password "password" and email "user@domain.com"
     Given there is a registered volunteer with username "volunteer" and password "password" and email "volunteer@domain.com"
-    Given there are pets in a shelter
+    And There is a Pet with breed "cat"
+    And There is a Pet with breed "dog"
 
-
-  Scenario: View Pets Catalogue as a Client
-    Given I login as "client" with password "password"
-    When I request the Catalogue
-    Then The response code is 200
 
   Scenario: View Pets Catalogue without being logged in
     Given I'm not logged in
+    When I request the Catalogue
+    Then The response code is 200
+
+    Scenario: View Pets Catalogue for a specific type of pet without being logged in
+      Given I'm not logged in
+      When I request the Catalogue with breed "cat"
+      Then The response code is 200
+
+  Scenario: View Pets Catalogue as a Client
+    Given I login as "client" with password "password"
     When I request the Catalogue
     Then The response code is 200
 
