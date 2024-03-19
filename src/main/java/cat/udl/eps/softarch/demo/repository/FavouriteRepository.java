7467bb018e04d42c1dc9767427ce7b35807c479c
Antt02 <antoniolopez18022002@gmail.com>
2024-03-19 19:38:24 +0100

    checking if failure comes from pushing repository and base class at the same time

--- src/main/java/cat/udl/eps/softarch/demo/repository/FavouriteRepository.java
+++
@@ -1,8 +0,0 @@
-package cat.udl.eps.softarch.demo.repository;
-
-import cat.udl.eps.softarch.demo.domain.Favourite;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface FavouriteRepository extends CrudRepository<Favourite, Long>, PagingAndSortingRepository<Favourite, Long> {
-}
