4317fe0500bd0da52b9fe2fdf5873f93838d09f6
vGerJ02 <gjaimejuanguardia@gmail.com>
2024-03-20 18:24:02 +0100

    refactor: optimize imports and case of given created shelter

--- src/test/java/cat/udl/eps/softarch/demo/steps/DeleteShelterStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/DeleteShelterStepDefs.java
@@ -1,26 +1,16 @@
 package cat.udl.eps.softarch.demo.steps;
 
-import cat.udl.eps.softarch.demo.domain.Admin;
 import cat.udl.eps.softarch.demo.domain.Shelter;
-import cat.udl.eps.softarch.demo.repository.AdminRepository;
 import cat.udl.eps.softarch.demo.repository.ShelterRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import netscape.javascript.JSObject;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
-
-import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 public class DeleteShelterStepDefs {
 
@@ -33,13 +23,15 @@ public class DeleteShelterStepDefs {
 
     @Given("There is a created shelter with name {string}, email {string} and phone {string}")
     public void thereIsACreatedShelterWithNameEmailAndPhone(String name, String email, String phone) {
-        Shelter shelter = new Shelter();
-        shelter.setName(name);
-        shelter.setEmail(email);
-        shelter.setMobile(phone);
-        shelter.setLocatedAt(null);
+        if (shelterRepository.findByName(name).isEmpty()) {
+            Shelter shelter = new Shelter();
+            shelter.setName(name);
+            shelter.setEmail(email);
+            shelter.setMobile(phone);
+            shelter.setLocatedAt(null);
 
-        shelterRepository.save(shelter);
+            shelterRepository.save(shelter);
+        }
     }
 
     @When("I try to delete Shelter with name {string}")
@@ -47,10 +39,10 @@ public class DeleteShelterStepDefs {
         List<Shelter> shelterList = shelterRepository.findByName(name);
 
         stepDefs.result = stepDefs.mockMvc.perform(
-                delete("/shelters/{id}", shelterList.isEmpty() ? "0" : shelterList.get(0).getId())
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print()) ;
+                        delete("/shelters/{id}", shelterList.isEmpty() ? "0" : shelterList.get(0).getId())
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
     }
 
 
