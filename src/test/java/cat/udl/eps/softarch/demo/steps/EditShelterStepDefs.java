4a58258a28a55a7faf2075ba6bb10aeab1216bb4
Sebastian Jitaru <sjitaru29@gmail.com>
2024-03-24 18:28:45 +0100

    New Scenarios implemeted

--- src/test/java/cat/udl/eps/softarch/demo/steps/EditShelterStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/EditShelterStepDefs.java
@@ -1,29 +1,90 @@
 package cat.udl.eps.softarch.demo.steps;
 
 import cat.udl.eps.softarch.demo.domain.Shelter;
+import cat.udl.eps.softarch.demo.domain.ShelterVolunteer;
 import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.domain.Admin;
+
+import cat.udl.eps.softarch.demo.repository.AdminRepository;
 import cat.udl.eps.softarch.demo.repository.ShelterRepository;
+import cat.udl.eps.softarch.demo.repository.ShelterVolunteerRepository;
+import cat.udl.eps.softarch.demo.repository.UserRepository;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 public class EditShelterStepDefs {
 
     @Autowired
     ShelterRepository shelterRepository;
-    User user = new User();
-    @And("^There a shelter with name \"([^\"]*)\" email \"([^\"]*)\" and mobile \"([^\"]*)\"$")
-    public void thereAShelterWithNameEmailAndMobile(String name, String email, String mobile) {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    AdminRepository adminRepository;
+    @Autowired
+    ShelterVolunteerRepository shelterVolunteerRepository;
+    @Autowired
+    StepDefs stepDefs;
+    @And("^There is already a shelter with name \"([^\"]*)\" email \"([^\"]*)\" and mobile \"([^\"]*)\"$")
+    public void thereIsAlreadyAShelterWithNameEmailAndMobile(String name, String email, String mobile) {
         Shelter shelter = new Shelter();
         shelter.setName(name);
         shelter.setEmail(email);
         shelter.setMobile(mobile);
         shelterRepository.save(shelter);
-
-        user.getAuthorities()
     }
 
-    @When("I update the shelter with name \"([^\"]*)\" to name \"([^\"]*)\" email \"([^\"]*)\" and mobile \"([^\"]*)\"")
-    public void iUpdateTheShelterWithNameToNameEmailAndMobile(String PrevName, String NewName, String email, String mobile) {
+    @When("^I update the shelter with name \"([^\"]*)\" to name \"([^\"]*)\" email \"([^\"]*)\" and mobile \"([^\"]*)\"$")
+    public void iUpdateTheShelterWithNameToNameEmailAndMobile(String PrevName, String NewName, String email, String mobile)
+        throws Throwable {
+                    Shelter shelter = shelterRepository.findByName(PrevName).get(0);
+                    if (shelter != null) {
+                       shelter =  shelterRepository.findByName(PrevName).get(0);
+                    }
+                stepDefs.result = stepDefs.mockMvc.perform(patch(shelter.getUri())//"/routes/"+route.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new JSONObject().put("name",NewName).put("email",email).put("mobile",mobile).put("createdAt", ZonedDateTime.now()).toString())
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+        }
+
+
+    @Given("^There is a registered admin with username \"([^\"]*)\" and password \"([^\"]*)\" and email \"([^\"]*)\"$")
+    public void thereIsARegisteredAdminWithUsernameAndPasswordAndEmail(String adminname, String adminPasswd, String adminEmail) throws Throwable{
+            if (!adminRepository.existsById(adminname)) {
+                Admin user = new Admin();
+                user.setEmail(adminEmail);
+                user.setId(adminname);
+                user.setPassword(adminPasswd);
+                user.encodePassword();
+                adminRepository.save(user);
+              
+        }
+    }
+
+    @Given("^There is a registered volunteer with username \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void thereIsARegisteredVolunteerWithUsernameAndPasswordAndEmail(String name, String mobile) {
+        if (!shelterVolunteerRepository.existsById(name)) {
+            ShelterVolunteer volunteer = new ShelterVolunteer();
+            volunteer.setEmail("shelter@sample.com");
+            volunteer.setId(name);
+            volunteer.setPassword(mobile);
+            volunteer.encodePassword();
+            shelterVolunteerRepository.save(volunteer);
+        }
     }
 }
--- src/test/resources/features/EditShelter.feature
+++ src/test/resources/features/EditShelter.feature
@@ -5,12 +5,47 @@ Feature: Edit Shelter
 
   Background:
     Given There is a registered user with username "user" and password "existing" and email "user@sample.app"
-    And There a shelter with name "Shelter 1" email "shelter@sample.com" and mobile "999999999"
+    Given There is a registered admin with username "admin" and password "admin" and email "admin@smaple.app"
+    Given There is a registered volunteer with username "volunteer" and password "volunteer"
+    And There is already a shelter with name "Shelter 1" email "shelter@sample.com" and mobile "999999999"
 
-  Scenario: Edit shelter:
-    Given I login as "username" with password "password"
-    And I am shelter volunteer
-    When I update the shelter with name "Shelter 1" to name "Another Shelter" email "another email" and mobile "another mobile"
+  Scenario: Edit shelter without being logged in
+    Given I'm not logged in
+    When I update the shelter with name "Shelter 1" to name "Another Shelter" email "shelter@sample.app" and mobile "123123123"
+    Then The response code is 401
+    And The error message is "Unauthorized"
+
+  Scenario: Edit shelter with user
+    Given I login as "client" with password "existing"
+    When I update the shelter with name "Shelter 1" to name "Another Shelter" email "shelter@sample.app" and mobile "123123123"
+    Then The response code is 401
+    And The error message is "Unauthorized"
+
+  Scenario: Edit shelter with admin
+    Given I login as "admin" with password "admin"
+    When I update the shelter with name "Shelter 1" to name "Another Shelter" email "shelter@sample.app" and mobile "123123123"
     Then The response code is 200
 
+  Scenario: Edit shelter with volunteer
+    Given I login as "volunteer" with password "volunteer"
+    When I update the shelter with name "Shelter 1" to name "Another Shelter" email "shelter@sample.app" and mobile "123123123"
+    Then The response code is 200
+
+  Scenario: Edit shelter with missing new name
+    Given I login as "admin" with password "admin"
+    When I update the shelter with name "Shelter 1" to name "" email "shelter@sample.app" and mobile "123123123"
+    Then The response code is 400
+    And The error message is "must not be blank"
+
+  Scenario: Edit shelter with missing new email
+    Given I login as "admin" with password "admin"
+    When I update the shelter with name "Shelter 1" to name "Another Shelter" email "" and mobile "123123123"
+    Then The response code is 400
+    And The error message is "must not be blank"
+
+  Scenario: Edit shelter with missing new mobile
+    Given I login as "admin" with password "admin"
+    When I update the shelter with name "Shelter 1" to name "Another Shelter" email "shelter@sample.app" and mobile ""
+    Then The response code is 400
+    And The error message is "must not be blank"
 
