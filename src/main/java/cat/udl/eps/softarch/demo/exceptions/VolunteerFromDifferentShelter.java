04c0a4ab4b9dcf07136957a1bc344f23bd7a308c
Pol Escolà Curcó <polescola3000@gmail.com>
2024-06-05 20:23:58 +0200

    New exception added

--- src/main/java/cat/udl/eps/softarch/demo/exceptions/VolunteerFromDifferentShelter.java
+++ src/main/java/cat/udl/eps/softarch/demo/exceptions/VolunteerFromDifferentShelter.java
@@ -3,3 +3,4 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.PRECONDITION_FAILED, reason = "El usuario debe ser de tipo shelterVolunteer")
 public class VolunteerFromDifferentShelter extends RuntimeException { }
+
